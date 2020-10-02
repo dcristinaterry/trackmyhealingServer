2b31c5b1da8a6e4b08493e4b693386a8fea16152
dcristinaterry <crisdc88@gmail.com>
2020-10-02 13:41:15 -0500

    fix .gitignore

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+module.exports={
+    session:{
+        cookieKey: "healing"
+    },
+    // GOOGLE:{
+    //     consumerKey:"833667121511-bfhhs616kln3bucu245btkd9l5qvr2c6.apps.googleusercontent.com",
+    //     consumerSecret: "-AWqWKtmKuZr1y-S-AL9aplz"
+    // },
+    // FACEBOOK:{
+        
+    // }
+
+}
